bca2e81ac8a24c81da87547ab103f3d072d17ca8
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2024-03-18 19:59:05 +0330

    Add File | Added Course and StudentCourseDetail classes

---
+++ src/main/java/Courses/Course.java
@@ -0,0 +1,12 @@
+package Courses;
+
+import java.util.UUID;
+
+public class Course
+{
+    //region Fields
+    protected UUID courseID;
+    protected String name;
+    protected UUID teacherID;
+    //endregion
+}
---
+++ src/main/java/Courses/StudentCourseDetail.java
@@ -0,0 +1,14 @@
+package Courses;
+
+import java.util.UUID;
+
+public class StudentCourseDetail
+{
+    //region Fields
+    private UUID studentCourseDetailID;
+    private UUID courseID;
+    private UUID studentID;
+    private int grade;
+    private boolean hasBeenGraded;
+    //endregion
+}
